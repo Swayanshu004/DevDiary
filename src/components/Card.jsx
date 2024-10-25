51a9e5997090219ac6edd68f7e363b75b4f569b4
Swayanshu004 <swayanshusahoo.pri@gmail.com>
2024-10-25 12:04:28 +0530

    prepare for deploy

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ function Card({title, creator, blogUrl, imageUrl}) {
   return (
     <div className='w-5/6 md:w-72 bg-neutral-200 max-h-60 rounded-lg hover:bg-lime-500 overflow-hidden'>
         <Link href={blogUrl ? blogUrl : ""}>
-            <img src="https://images.pexels.com/photos/28975090/pexels-photo-28975090/free-photo-of-tranquil-boat-ride-on-yamuna-river-at-dusk.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="na" className='w-full aspect-video '/>
+            <img src={imageUrl} alt="na" className='w-full aspect-video '/>
             <h3 className='text-black font-semibold text-xl px-3 py-1 font-mono'>{title}</h3>
             <div className='flex items-center justify-between px-3'>
               <p className='text-neutral-600 font-medium '>{creator}</p>
