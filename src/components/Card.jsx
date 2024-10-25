e6bad1a38c22f24adfbfd5b7ba49be2c5094b1b9
Swayanshu Satyapragyan Sahoo <125992313+Swayanshu004@users.noreply.github.com>
2024-10-25 23:55:28 +0530

    prepare for deploy

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ function Card({title, creator, blogUrl, imageUrl}) {
         <Link href={blogUrl ? blogUrl : ""}>
             <img src={imageUrl ? imageUrl : "https://res.cloudinary.com/swayanshu/image/upload/v1729875132/DevDiary/k165zbyingeopsi4dztm.png"} alt="na" className='w-full aspect-video '/>
             <h3 className='text-black font-semibold text-xl px-3 py-1 font-mono'>{title}</h3>
-            <div className='flex items-center justify-between px-3'>
+            <div className='flex items-center justify-between px-3 pb-4'>
               <p className='text-neutral-600 font-medium '>{creator}</p>
               {blogUrl ? "" : <p className='text-neutral-500 font-bold px-3 border-2 bg-neutral-200 border-lime-500 rounded-md'>writing</p>}
             </div>
