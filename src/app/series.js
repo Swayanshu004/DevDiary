4bcc12bc464ecc5e8c1227e41d269334080471d6
Swayanshu004 <swayanshusahoo.pri@gmail.com>
2025-10-02 14:58:44 +0530

    LLM thubnail

--- src/app/series.js
+++ src/app/series.js
@@ -2,7 +2,7 @@ const series = [
     {
       "_id": "27f946f3add0804693e4dc49258b7eba",
       "title": "Large Language Model",
-      "imageUrl": "https://res.cloudinary.com/swayanshu/image/upload/v1759396198/DevDiary/LLM_wxjgp9.png",
+      "imageUrl": "https://res.cloudinary.com/swayanshu/image/upload/v1759397297/DevDiary/LLM_mxu1m0.png",
       "url": "/series/27f946f3add0804693e4dc49258b7eba",
       "creatorName": "swayanshu",
       "tokens":["Large","Language","Model","AI","LLM",],
